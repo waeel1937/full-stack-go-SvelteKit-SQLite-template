163aae190b7d91f031b89fe596dfe704fc61dc84
waeel1937 <waeelkhashfeh@yahoo.com>
2026-02-27 15:05:42 +0100

    add

--- edge-app/scripts/build-arm64.sh
+++ edge-app/scripts/build-arm64.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -e
+mkdir -p dist
 GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o dist/edge-app ./cmd/edge-app
--- edge-app/scripts/build-armv7.sh
+++ edge-app/scripts/build-armv7.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -e
+mkdir -p dist
 GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=0 go build -o dist/edge-app ./cmd/edge-app
